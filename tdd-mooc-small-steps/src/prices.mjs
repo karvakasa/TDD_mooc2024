fbde1b348d633aeb2b9e6a3087b53bafb48da0f4
Tallberg Topias <topias.tallberg@gmail.com>
2024-03-26 15:07:31 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -82,7 +82,7 @@ function createApp(database) {
   }
 
   function isMonday(date, date2) {
-    return date.getDay() === 1;
+    return date2.dayOfWeek === 1;
   }
 
   function isHoliday(date, date2) {
