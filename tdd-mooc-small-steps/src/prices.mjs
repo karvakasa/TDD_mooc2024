8aa21c6d3fadd96acd98f71f1a469df49676afb9
Tallberg Topias <topias.tallberg@gmail.com>
2024-03-26 15:26:46 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -79,12 +79,12 @@ function createApp(database) {
     return date2.dayOfWeek === 1;
   }
 
-  function isHoliday(date2) {
+  function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = Temporal.PlainDate.from(row.holiday);
       if (
-        date2.equals(holiday)
+        date.equals(holiday)
       ) {
         return true;
       }
